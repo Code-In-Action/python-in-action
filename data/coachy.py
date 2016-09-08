1676cd32e3f151faf4c9c1e229125faab06f8af7
Go7hic <gtfx0209@qq.com>
2016-09-09 00:42:53 +0800

    update data/

--- data/coachy.py
+++ data/coachy.py
@@ -1,22 +1,32 @@
 #!/usr/local/bin/python
 #coding=utf-8
-with open('james.txt') as jaf:
-    data = jaf.readline()
-james = data.strip().split(',')
+# 集合 set, 排序
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return (time_string)
+    (mins, secs) = time_string.split(splitter)
+    return (mins+'.'+secs)
 
-with open('julie.txt') as juf:
-    data = juf.readline()
-julie = data.strip().split(',')
+def get_coach_data(filename):
+    try:
+        with open(filename) as f:
+            data = f.readline()
+        return (data.strip().split(','))
+    except IOError as ioerr:
+        print('File error'+str(ioerr))
+        return(None)
 
-with open('mikey.txt') as mif:
-    data = mif.readline()
-mikey = data.strip().split(',')
+james = get_coach_data('james.txt')
+julie = get_coach_data('julie.txt')
+mikey = get_coach_data('mikey.txt')
+sarah = get_coach_data('sarah.txt')
 
-with open('sarah.txt') as saf:
-    data = saf.readline()
-sarah = data.strip().split(',')
+print(sorted(set([sanitize(t) for t in james]))[0:3])
+print(sorted(set([sanitize(t) for t in julie]))[0:3])
+print(sorted(set([sanitize(t) for t in mikey]))[0:3])
+print(sorted(set([sanitize(t) for t in sarah]))[0:3])
 
-print(sorted(james))
-print(sorted(julie))
-print(sorted(mikey))
-print(sorted(sarah))
