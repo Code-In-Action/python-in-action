a9c9d8f56c6530cb20241711d2b9ff042c548c30
Go7hic <gtfx0209@qq.com>
2016-11-03 23:31:30 +0800

    add webscrap

--- webscrap/c1.py
+++ webscrap/c1.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
+
 import urllib.request
-def download(url, num_retries = 2):
+import re
+
+
+# 下载网页
+def download(url, user_agent='wswp', num_retries = 2):
     """Download function that also retries 5XX errors"""
     print('Downloading:'+ url)
+    headers = {'User-agent': user_agent}
+    # 设置代理
+    request = urllib.request.Request(url, headers=headers)
     try:
-        html = urllib.request.urlopen(url).read()
+        html = urllib.request.urlopen(request).read()
     except urllib.request.URLError as e:
         print('Download error:'+ e.reason)
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 # retry 5XX HTTP errors
-                html = download(url, num_retries - 1)
+                html = download(url,user_agent, num_retries - 1)
     return html
 
-
-download('http://baidu.com')
+# 网站地图爬虫
+def crawl_sitemap(url):
+    sitemap = download(url)
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    for links in links:
+        html = download(links)
+if __name__ == '__main__':
+    print(download('http://www.v2ex.com/go/qna'))
+    print(crawl_sitemap('http://example.webscraping.com/sitemap.xml'))
