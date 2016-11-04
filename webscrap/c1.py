ebf3f654d75371a72f336f4c6f88bcc609587ed1
Go7hic <gtfx0209@qq.com>
2016-11-05 03:14:35 +0800

    update webscrap

--- webscrap/c1.py
+++ webscrap/c1.py
@@ -1,33 +1,162 @@
-# -*- coding: utf-8 -*-
-
-import urllib.request
 import re
+# from urllib.parse import urlparse
+# from urllib.parse import urljoin
+# from urllib.parse import urldefrag
+import urllib
+import time
+from datetime import datetime
+from urllib.robotparser import RobotFileParser
+import queue
 
+import csv
+import lxml.html
+
+# 链接爬虫
+def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, headers=None, user_agent='wswp', proxy=None, num_retries=1, scrape_callback=None):
+    """Crawl from the given seed URL following links matched by link_regex
+    """
+    # the queue of URL's that still need to be crawled
+    crawl_queue = [seed_url]
+    # the URL's that have been seen and at what depth
+    seen = {seed_url: 0}
+    # track how many URL's have been downloaded
+    num_urls = 0
+    rp = get_robots(seed_url)
+    throttle = Throttle(delay)
+    headers = headers or {}
+    if user_agent:
+        headers['User-agent'] = user_agent
+
+    while crawl_queue:
+        url = crawl_queue.pop()
+        depth = seen[url]
+        # check url passes robots.txt restrictions
+        if rp.can_fetch(user_agent, url):
+            throttle.wait(url)
+            html = download(url, headers, proxy=proxy, num_retries=num_retries)
+            links = []
+            if scrape_callback:
+                links.extend(scrape_callback(url, html) or [])
+            # 避免爬虫陷进
+            if depth != max_depth:
+                # can still crawl further
+                if link_regex:
+                    # filter for links matching our regular expression
+                    links.extend(link for link in get_links(html) if re.match(link_regex, link))
+
+                for link in links:
+                    link = normalize(seed_url, link)
+                    # check whether already crawled this link
+                    if link not in seen:
+                        seen[link] = depth + 1
+                        # check link is within same domain
+                        if same_domain(seed_url, link):
+                            # success! add this new link to queue
+                            crawl_queue.append(link)
+
+            # check whether have reached downloaded maximum
+            num_urls += 1
+            if num_urls == max_urls:
+                break
+        else:
+            print('Blocked by robots.txt:'+ url)
+
+
+# 下载限速
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+
+    def wait(self, url):
+        domain = urllib.parse.urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
 
 # 下载网页
-def download(url, user_agent='wswp', num_retries = 2):
-    """Download function that also retries 5XX errors"""
-    print('Downloading:'+ url)
-    headers = {'User-agent': user_agent}
-    # 设置代理
-    request = urllib.request.Request(url, headers=headers)
+def download(url, headers, proxy, num_retries, data=None):
+    print('Downloading:', url)
+    request = urllib.request.Request(url, data, headers)
+    opener = urllib.request.build_opener()
+    # 支持代理
+    if proxy:
+        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
+        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
     try:
-        html = urllib.request.urlopen(request).read()
-    except urllib.request.URLError as e:
-        print('Download error:'+ e.reason)
-        html = None
-        if num_retries > 0:
-            if hasattr(e, 'code') and 500 <= e.code < 600:
+        response = opener.open(request)
+        html = response.read()
+        code = response.code
+    except request.URLError as e:
+        print('Download error:', e.reason)
+        html = ''
+        if hasattr(e, 'code'):
+            code = e.code
+            if num_retries > 0 and 500 <= code < 600:
                 # retry 5XX HTTP errors
-                html = download(url,user_agent, num_retries - 1)
+                return download(url, headers, proxy, num_retries - 1, data)
+        else:
+            code = None
     return html
 
-# 网站地图爬虫
-def crawl_sitemap(url):
-    sitemap = download(url)
-    links = re.findall('<loc>(.*?)</loc>', sitemap)
-    for links in links:
-        html = download(links)
+
+def normalize(seed_url, link):
+    """Normalize this URL by removing hash and adding domain
+    """
+    link, _ = urllib.parse.urldefrag(link)  # remove hash to avoid duplicates
+    return urllib.parse.urljoin(seed_url, link)
+
+
+def same_domain(url1, url2):
+    """Return True if both URL's belong to same domain
+    """
+    return urllib.parse.urlparse(url1).netloc == urllib.parse.urlparse(url2).netloc
+
+# 解析robotx.txt
+def get_robots(url):
+    """Initialize robots parser for this domain
+    """
+    rp = RobotFileParser()
+    rp.set_url(urllib.parse.urljoin(url, '/robots.txt'))
+    rp.read()
+    return rp
+
+
+def get_links(html):
+    """Return a list of links from html
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    html = html.decode('utf-8')
+    return webpage_regex.findall(html)
+
+# 抓取回调的回调类,把得到的数据存到csv表格里
+class ScrapeCallback:
+    def __init__(self):
+        self.writer = csv.writer(open('countries.csv', 'w'))
+        self.fields = ('area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone', 'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
+        self.writer.writerow(self.fields)
+
+    def __call__(self, url, html):
+        if re.search('/view/', url):
+
+            tree = lxml.html.fromstring(html)
+            row = []
+            for field in self.fields:
+                row.append(tree.cssselect('table > tr#places_{}__row > td.w2p_fw'.format(field))[0].text_content())
+            self.writer.writerow(row)
+
 if __name__ == '__main__':
-    print(download('http://www.v2ex.com/go/qna'))
-    print(crawl_sitemap('http://example.webscraping.com/sitemap.xml'))
+    link_crawler('http://example.webscraping.com/', '/(index|view)', delay=0, num_retries=1,max_depth=1,scrape_callback=ScrapeCallback())
+    link_crawler('https://www.lagou.com/jobs/list_%E5%89%8D%E7%AB%AF%E5%BC%80%E5%8F%91?px=new&city=%E6%9D%AD%E5%B7%9E#order', delay=0, num_retries=1,max_depth=1,scrape_callback=ScrapeCallback())
+
