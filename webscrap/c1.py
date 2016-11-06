61f9821be90fdd7279c161145b90a1f3b815726d
Go7hic <gtfx0209@qq.com>
2016-11-06 21:33:21 +0800

    update webscrap

--- webscrap/c1.py
+++ webscrap/c1.py
@@ -7,12 +7,20 @@ import time
 from datetime import datetime
 from urllib.robotparser import RobotFileParser
 import queue
+import random
+import socket
 
 import csv
 import lxml.html
 
+DEFAULT_AGENT = 'wswp'
+DEFAULT_DELAY = 5
+DEFAULT_RETRIES = 1
+DEFAULT_TIMEOUT = 60
+
+
 # 链接爬虫
-def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, headers=None, user_agent='wswp', proxy=None, num_retries=1, scrape_callback=None):
+def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, user_agent='wswp', proxies=None, num_retries=1, scrape_callback=None, cache=None):
     """Crawl from the given seed URL following links matched by link_regex
     """
     # the queue of URL's that still need to be crawled
@@ -22,18 +30,13 @@ def link_crawler(seed_url, link_regex=None, delay=5, max_depth=-1, max_urls=-1, 
     # track how many URL's have been downloaded
     num_urls = 0
     rp = get_robots(seed_url)
-    throttle = Throttle(delay)
-    headers = headers or {}
-    if user_agent:
-        headers['User-agent'] = user_agent
-
+    D = Downloader(delay=delay, user_agent=user_agent, proxies=proxies, num_retries=num_retries, cache=cache)
     while crawl_queue:
         url = crawl_queue.pop()
         depth = seen[url]
         # check url passes robots.txt restrictions
         if rp.can_fetch(user_agent, url):
-            throttle.wait(url)
-            html = download(url, headers, proxy=proxy, num_retries=num_retries)
+            html = D(url)
             links = []
             if scrape_callback:
                 links.extend(scrape_callback(url, html) or [])
@@ -83,30 +86,63 @@ class Throttle:
                 time.sleep(sleep_secs)
         self.domains[domain] = datetime.now()
 
+
 # 下载网页
-def download(url, headers, proxy, num_retries, data=None):
-    print('Downloading:', url)
-    request = urllib.request.Request(url, data, headers)
-    opener = urllib.request.build_opener()
-    # 支持代理
-    if proxy:
-        proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
-        opener.add_handler(urllib.request.ProxyHandler(proxy_params))
-    try:
-        response = opener.open(request)
-        html = response.read()
-        code = response.code
-    except request.URLError as e:
-        print('Download error:', e.reason)
-        html = ''
-        if hasattr(e, 'code'):
-            code = e.code
-            if num_retries > 0 and 500 <= code < 600:
-                # retry 5XX HTTP errors
-                return download(url, headers, proxy, num_retries - 1, data)
-        else:
-            code = None
-    return html
+class Downloader:
+    def __init__(self, delay=DEFAULT_DELAY, user_agent=DEFAULT_AGENT, proxies=None, num_retries=DEFAULT_RETRIES, timeout=DEFAULT_TIMEOUT, opener=None, cache=None):
+        socket.setdefaulttimeout(timeout)
+        self.throttle = Throttle(delay)
+        self.user_agent = user_agent
+        self.proxies = proxies
+        self.num_retries = num_retries
+        self.opener = opener
+        self.cache = cache
+
+    def __call__(self, url):
+        result = None
+        # 如果有缓存,不限速
+        if self.cache:
+            try:
+                result=self.cache[url]
+            except KeyError:
+                pass
+            else:
+                if self.num_retries > 0 and 500 <= result['code'] < 600:
+                    result = None
+        if result is None:
+            # result was not loaded from cache so still need to download
+            self.throttle.wait(url)
+            proxy = random.choice(self.proxies) if self.proxies else None
+            headers = {'User-agent': self.user_agent}
+            result = self.download(url, headers, proxy=proxy, num_retries=self.num_retries)
+            if self.cache:
+                # save result to cache
+                self.cache[url] = result
+        return result['html']
+
+    def download(self, url, headers, proxy, num_retries, data=None):
+        print('Downloading:', url)
+        request = urllib.request.Request(url, data, headers)
+        opener = urllib.request.build_opener()
+        # 支持代理
+        if proxy:
+            proxy_params = {urllib.parse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib.request.ProxyHandler(proxy_params))
+        try:
+            response = opener.open(request)
+            html = response.read()
+            code = response.code
+        except Exception as e:
+            print('Download error:', e.reason)
+            html = ''
+            if hasattr(e, 'code'):
+                code = e.code
+                if num_retries > 0 and 500 <= code < 600:
+                    # retry 5XX HTTP errors
+                    return self._get(url, headers, proxy, num_retries - 1, data)
+            else:
+                code = None
+        return {'html': html, 'code': code}
 
 
 def normalize(seed_url, link):
@@ -120,6 +156,7 @@ def same_domain(url1, url2):
     """Return True if both URL's belong to same domain
     """
     return urllib.parse.urlparse(url1).netloc == urllib.parse.urlparse(url2).netloc
+
 
 # 解析robotx.txt
 def get_robots(url):
@@ -139,6 +176,7 @@ def get_links(html):
     # list of all links from the webpage
     html = html.decode('utf-8')
     return webpage_regex.findall(html)
+
 
 # 抓取回调的回调类,把得到的数据存到csv表格里
 class ScrapeCallback:
